352a61485a2d10383e57ef980768b3a22ad4d140
Siddhartha Paul <siddharthapaul440@gmail.com>
2024-07-05 19:33:50 +0530

    05/07/2024

--- src/app/dashboard/add-student/add-student.component.ts
+++ src/app/dashboard/add-student/add-student.component.ts
@@ -1,5 +1,7 @@
 import { Component } from '@angular/core';
 import { RouteStatusService } from '../../services/route-status.service';
+import { FormBuilder, FormGroup, Validators } from '@angular/forms';
+import { StudentService } from '../../services/student.service';
 
 @Component({
   selector: 'app-add-student',
@@ -8,7 +10,7 @@ import { RouteStatusService } from '../../services/route-status.service';
 })
 export class AddStudentComponent {
 
-  constructor(private routeStatusService: RouteStatusService){
+  constructor(private routeStatusService: RouteStatusService, private fb: FormBuilder, private studentService: StudentService){
     this.routeStatusService.hideHeader = true;
   }
 
