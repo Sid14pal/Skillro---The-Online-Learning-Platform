82745bb19aa738cca32fe735e4286dc06bc34cc4
Siddhartha Paul <siddharthapaul440@gmail.com>
2025-09-05 16:33:56 +0530

    updated video player

--- src/app/dashboard/play-video/play-video.component.ts
+++ src/app/dashboard/play-video/play-video.component.ts
@@ -1,23 +1,27 @@
-import { Component, AfterViewInit, OnDestroy } from '@angular/core';
+import { Component, OnInit, Inject, PLATFORM_ID } from '@angular/core';
 import { ActivatedRoute } from '@angular/router';
+import { isPlatformBrowser } from '@angular/common';
+import videojs from 'video.js';
+import type Player from 'video.js/dist/types/player';
 
 @Component({
   selector: 'app-play-video',
   templateUrl: './play-video.component.html',
-  styleUrl: './play-video.component.css'
+  styleUrls: ['./play-video.component.css'] // ✅ fixed typo: should be styleUrls
 })
-export class PlayVideoComponent {
+export class PlayVideoComponent implements OnInit {
 
   videoUrl: string | null = null;
+  player!: Player;
+  isBrowser = false;
 
-  constructor(private route: ActivatedRoute) {}
+  constructor(private route: ActivatedRoute, @Inject(PLATFORM_ID) private platformId: Object) {
+    this.isBrowser = isPlatformBrowser(this.platformId);
+  }
 
   ngOnInit(): void {
     this.route.queryParams.subscribe(params => {
       this.videoUrl = params['video'];
-      console.log(this.videoUrl)
     });
   }
-
-
 }
